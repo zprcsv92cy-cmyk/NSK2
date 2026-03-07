d090f756e6a407798cb0d44c4204c82e93faa262
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-03-08 00:20:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-
 const CACHE_NAME="nsk-team18-v73";
-const ASSETS=["./","./index.html","./app.css","./auth.js","./db.js","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const ASSETS=["./","./index.html","./config.js?v=73","./app.css?v=73","./auth.js?v=73","./db.js?v=73","./app.js?v=73","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))),self.clients.claim()]));});
 self.addEventListener("fetch",event=>{
