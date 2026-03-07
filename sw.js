1610b255715c67d7c7c4681f514db1db2ca266cd
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-03-07 23:01:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('install',e=>self.skipWaiting())
+
+const CACHE_NAME="nsk-team18-v72";
+const ASSETS=["./","./index.html","./app.css","./auth.js","./db.js","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
+self.addEventListener("activate",e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))),self.clients.claim()]));});
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.method!=="GET") return;
+  event.respondWith((async()=>{
+    const cached=await caches.match(req);
+    if(cached) return cached;
+    const fresh=await fetch(req);
+    const cache=await caches.open(CACHE_NAME);
+    cache.put(req,fresh.clone());
+    return fresh;
+  })());
+});
