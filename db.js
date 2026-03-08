f286ea94a58348e3c47ef92f5eb94d2c58516eeb
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-03-08 21:33:55 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
-window.DB = (()=>{
+window.DB=(()=>{
 const client=()=>Auth.getClient();
 
 async function listPlayers(){
- const {data}=await client().from("nsk_players").select("*").order("full_name");
- return data||[];
+const {data}=await client().from("nsk_players").select("*").order("full_name");
+return data||[];
 }
 
 async function addPlayer(name){
- await client().from("nsk_players").insert({full_name:name});
+await client().from("nsk_players").insert({full_name:name});
 }
 
 async function listPools(){
- const {data}=await client().from("nsk_pools").select("*").order("pool_date");
- return data||[];
+const {data}=await client().from("nsk_pools").select("*").order("pool_date");
+return data||[];
 }
 
 async function addPool(p){
- await client().from("nsk_pools").insert(p);
+await client().from("nsk_pools").insert(p);
 }
 
-return {listPlayers,addPlayer,listPools,addPool};
+return{listPlayers,addPlayer,listPools,addPool};
 })();
