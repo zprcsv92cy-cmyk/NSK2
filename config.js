db43d2ede57bc45c8f1971d7600c1f227c99a584
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-03-09 00:32:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://tonbbmxzotsjwuimobkn.supabase.co",
-  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvbmJibXh6b3Rzand1aW1vYmtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODExMTIsImV4cCI6MjA4ODI1NzExMn0.lYByBwAhjbxlSYpOcv8W3JboNkf8ldTiieQ4goMtopc"
-REDIRECT_PATH: "/NSK2/"
-APP_VERSION: "v10-safe"
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvbmJibXh6b3Rzand1aW1vYmtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODExMTIsImV4cCI6MjA4ODI1NzExMn0.lYByBwAhjbxlSYpOcv8W3JboNkf8ldTiieQ4goMtopc",
+  REDIRECT_PATH: "/NSK2/",
+  APP_VERSION: "v10-safe"
 };
-
